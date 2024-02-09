37fb0640709b340e0208b6c9a775611a90dac761
Vitor Giacometti <vitorgiacometti@gmail.com>
2024-02-08 21:20:00 -0300

    Criando o token ERC20

---
+++ Aula-8/ownable.sol
@@ -0,0 +1,31 @@
+/*
+SPDX-License-Identifier: CC-BY-4.0
+(c) Desenvolvido por Jeff Prestes
+This work is licensed under a Creative Commons Attribution 4.0 International License.
+*/
+pragma solidity 0.8.19;
+
+@title Manages the contract owner
+contract Ownable {
+    address payable contractOwner;
+
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "only owner can perform this operation");
+        _;
+    }
+
+    constructor() { 
+        contractOwner = payable(msg.sender); 
+    }
+    
+    function whoIsTheOwner() public view returns(address) {
+        return contractOwner;
+    }
+
+    function changeOwner(address _newOwner) onlyOwner public returns (bool) {
+        require(_newOwner != address(0x0), "only valid address");
+        contractOwner = payable(_newOwner);
+        return true;
+    }
+    
+}
